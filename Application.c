398365534bb89851ef96787997fa82e7def191bf
stoneshaker <stoneshaker73@gmail.com>
2015-09-15 18:25:41 -0700

    Updated with comments

--- Application.c
+++ Application.c
@@ -4,14 +4,13 @@ void main(void)
 {
 	char dummy;
 	int j;
-	for (
-		j = 0; j < 101; j++) {
-		if (!(j % 3)) {
-			if (!(j % 5)) printf("Fizz and Buzz\n");
-			else printf("Fizz\n");
+	for (j = 0; j < 101; j++) {  // cycle through 0 to 100
+		if (!(j % 3)) { // check if index is divisible by 3
+			if (!(j % 5)) printf("Fizz and Buzz\n"); // check if index is also divisible by 5
+			else printf("Fizz\n"); // case only divisible by 3
 		}
-		else if (!(j % 5)) printf("Buzz\n");
-		else printf("%d\n",j);
+		else if (!(j % 5)) printf("Buzz\n"); // check if index is divisible by 5
+		else printf("%d\n",j); // default case - print index
 	}
-	dummy = getchar();
+	dummy = getchar();  // pause before exiting (for Windows)
 }
